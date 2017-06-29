6594520dca5b94cd1a9a98823c396de6f40c315f
Gina <paalginaa@gmail.com>
2017-06-29 15:50:46 +0200

    starting to implement the sign up and login system, htmls added

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+
+@app.route("/log-in")
+def log_in():
+    return render_template("log_in.html")
+
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
