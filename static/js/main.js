59ec3bee1f4661f9df66d027f69abf1af1d4696d
Gina <paalginaa@gmail.com>
2017-06-29 13:04:36 +0200

    first page data appears in table

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,41 @@
+function createNewRow (planetObj) {
+
+    var newRow = document.createElement("tr");
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['name'];
+    newRow.appendChild(newCell);
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['diameter']+'km';
+    newRow.appendChild(newCell);
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['climate'];
+    newRow.appendChild(newCell);
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['terrain'];
+    newRow.appendChild(newCell);
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['surface_water']+'%';
+    newRow.appendChild(newCell);
+
+    var newCell = document.createElement("td");
+    newCell.textContent = planetObj['population'];
+    newRow.appendChild(newCell);
+
+    return newRow
+};
+
 function result(planetList) {
     for (var i = 0; i < planetList.length; i++) {
         var planet = planetList[i];
+        var row = createNewRow(planet);
         console.log(planet);
+
+        document.getElementById("planetTable").appendChild(row);
     }
 }
 
