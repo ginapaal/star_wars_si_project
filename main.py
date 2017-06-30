41afb2f4b2b1302c7b9f45b66871aeb60dd3bd0d
Gina <paalginaa@gmail.com>
2017-06-30 16:15:12 +0200

    database stores hashed passwords

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import data_handler
 from conn_man import connect_to_db
+import pw_hash
 
 
 app = Flask(__name__)
@@ -21,6 +22,8 @@ def sign_up():
     if request.form.get("user-signup") and request.form.get("pwd-signup"):
         username = request.form.get("user-signup")
         password = request.form.get("pwd-signup")
+        password = pw_hash.hash_pw(password)
+        print(password)
         data_handler.sign_up(username, password, connect_to_db())
         return redirect(url_for("main"))
     
