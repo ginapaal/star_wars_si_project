a1d8971555ab40f93a5a807bed0fac4cbf04eb73
Gina <paalginaa@gmail.com>
2017-07-01 12:26:42 +0200

    Procfile, requirements and runtime added

--- conn_man.py
+++ conn_man.py
@@ -1,9 +1,12 @@
 import psycopg2
 import config
+import urllib
 
 
 def connect_to_db():
     try:
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
         conn = psycopg2.connect("dbname='{}' user='{}' host='localhost' password='{}'".format(
             config.db_name, config.user, config.password))
         conn.autocommit = True
